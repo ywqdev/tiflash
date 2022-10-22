659060fc7150e3a7d8a52de2b542e0b235dbfbcb
ywqzzy <592838129@qq.com>
2022-10-22 16:35:33 +0800

    close mintso

--- dbms/src/Flash/Mpp/MinTSOScheduler.cpp
+++ dbms/src/Flash/Mpp/MinTSOScheduler.cpp
@@ -49,8 +49,8 @@ MinTSOScheduler::MinTSOScheduler(UInt64 soft_limit, UInt64 hard_limit, UInt64 ac
     {
         if (thread_hard_limit <= thread_soft_limit || thread_hard_limit > OS_THREAD_SOFT_LIMIT) /// the general soft limit of OS threads is no more than 100000.
         {
-            thread_hard_limit = 10000;
-            thread_soft_limit = 5000;
+            thread_hard_limit = 0;
+            thread_soft_limit = 0;
             LOG_INFO(log, "hard limit {} should > soft limit {} and under maximum {}, so MinTSOScheduler set them as {}, {} by default, and active_set_soft_limit is {}.", hard_limit, soft_limit, OS_THREAD_SOFT_LIMIT, thread_hard_limit, thread_soft_limit, active_set_soft_limit);
         }
         else
